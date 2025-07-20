dab98b51309686b6e43116e1e7bd0c3a9b50ce2a
unknown <kathanmajithia@gmail.com>
2025-07-20 17:26:17 +0530

    Questions

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ const teams = [
     { name: "Null pointers", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
 ];
 
+const problemStatements = [
+    "Best Time to buy and sell stock",
+    "Merge K sorted Lists",
+    "Swap Nodes in pairs",
+    "Pascal's Triangle",
+    "Happy Number",
+    "Symmetric Tree",
+    "Path Sum",
+    "Odd Even Linked List",
+    "Valid Parenthesis String",
+    "Minimum moves to reach target score",
+    "Merge Nodes in Between Zeros",
+    "Insert Greatest Common divisors in linked list",
+    "Convert 1D to 2D array with conditions"
+];
+
 
 
