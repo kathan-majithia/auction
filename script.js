5698696081e5bba140242711365a3d50ef6e95e8
unknown <kathanmajithia@gmail.com>
2025-07-20 17:26:09 +0530

    Team details

---
+++ script.js
@@ -0,0 +1,11 @@
+const teams = [
+    { name: "Skill Issue", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+    { name: "Tech Titans", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+    { name: "Marvels", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+    { name: "Code Ninjas", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+    { name: "Strugglers", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+    { name: "Null pointers", remainingCoins: 100, question1Bid: 0, question2Bid: 0 },
+];
+
+
+
